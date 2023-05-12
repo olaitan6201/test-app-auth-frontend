8d62ad9591f951e29204540f5ea122d9f49b44c5
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-12 17:57:33 +0100

    Updated Auth component

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,4 +1,4 @@
-export default function Auth({ handleSubmit, children }: any) {
+export default function Auth({ handleSubmit, children, heading = '', subHeading = '' }: any) {
     return (
         <div className="flex md:grid md:grid-cols-4 lg:grid-cols-5 w-full h-screen">
             <div className="md:col-span-2 lg:col-span-3 object-contain hidden md:inline-grid">
@@ -6,8 +6,8 @@ export default function Auth({ handleSubmit, children }: any) {
             </div>
             <form className="flex-1 md:col-span-2 flex" onSubmit={handleSubmit}>
                 <div className="my-auto mx-5">
-                    <h1 className="text-2xl font-bold">Welcome back!</h1>
-                    <p className="my-3 text-sm font-medium text-gray-600">Sign in to your account and continue your journey towards improved productivity.</p>
+                    <h1 className="text-2xl font-bold">{heading}</h1>
+                    <p className="my-3 text-sm font-medium text-gray-600">{subHeading}</p>
 
                     <div className="my-3">
                         {children}
